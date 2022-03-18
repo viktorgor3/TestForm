bd7389b4792da23afac518804022eea290bd6630
viktorgor3 <viktorgor3@mail.ru>
2022-03-18 14:44:38 +0300

    create directory local/assets,move main from bitrix

---
+++ local/templates/main/header.php
@@ -0,0 +1,26 @@
+<?
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+	die();
+?>
+<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <?$APPLICATION->ShowHead();?>
+
+            <meta name="description" content="">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+
+
+            <title><?$APPLICATION->ShowTitle();?></title>
+        </head>
+        <body>
+        <div id="panel">
+            <?$APPLICATION->ShowPanel();?>
+        </div>
+        <div class="body-top"></div>
+        <div class="body-footer"></div>
+            <div class="header-area">
+                <h1 class="head">Форма обратной связи</h1>
+            </div>
